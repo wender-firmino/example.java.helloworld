5e0142f3ac918b15cdcb82911864ddaf334a1b10
wender-firmino <38659709+wender-firmino@users.noreply.github.com>
2019-04-04 17:07:08 -0300

    Update Main.java

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,9 +1,6 @@
-package HelloWorld;
-
 public class Main
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world");
+     public static void main(String[] args) {
+        System.out.println("Hello, World");
     }
 }
